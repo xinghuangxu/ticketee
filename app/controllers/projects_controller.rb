86c5c9ae2407cb916d15bc0c8bbd3de366dcb4ca
Xinghuang Xu <xinghuangxu@gmail.com>
2014-06-05 14:55:28 -0500

    catch not found exception

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,11 @@
 class ProjectsController < ApplicationController
+	before_action :set_project, only:[:show,:edit,:update,:destroy]
+
 	def index
 		@projects=Project.all
 	end
 
 	def show
-		@project=Project.find(params[:id])
 	end
 
 	def new
@@ -12,13 +13,10 @@ class ProjectsController < ApplicationController
 	end
 
 	def edit
-		@project=Project.find(params[:id])
 	end
 
 	def update
-		@project=Project.find(params[:id])
 		if(@project.update(project_params))
-
 			flash[:notice]="Project has been updated."
 			redirect_to @project
 		else
@@ -38,7 +36,23 @@ class ProjectsController < ApplicationController
 		end
 	end
 
+	def destroy
+		if @project.destroy
+			flash.notice="Project has been destroyed."
+		else
+			flash.alert="Project has not been destroyed.s"
+		end
+		redirect_to projects_path
+	end
+
 	private 
+	def set_project
+		@project=Project.find(params[:id])
+	rescue ActiveRecord::RecordNotFound
+		flash[:alert]="The project you were looking for could not be found."
+		redirect_to projects_path
+	end
+
 	def project_params
 		params.require(:project).permit(:name,:description)
 	end
