3d9c192401283cf024c8bcc239e92456b72e70cc
Xinghuang Xu <xinghuangxu@gmail.com>
2014-06-09 17:46:41 -0500

    ticket features

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -26,9 +26,19 @@ class TicketsController < ApplicationController
 	end
 
 	def update
+		if @ticket.update(ticket_params)
+			flash[:notice] = "Ticket has been updated."
+			redirect_to [@project, @ticket]
+		else
+			flash[:alert] = "Ticket has not been updated."
+			render action: "edit"
+		end
 	end
 
 	def destroy
+		@ticket.destroy
+		flash[:notice] = "Ticket has been deleted."
+		redirect_to @project
 	end
 	
 
